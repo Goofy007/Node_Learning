b6d7b7268bcc1aabf4c5255c201cf6c13d5bc3a2
许丹琦 <1204253728@qq.com>
2019-08-16 13:42:57 +0800

    copyImg

--- FS/Code/05-readImg.js
+++ FS/Code/05-readImg.js
@@ -1,4 +1,16 @@
 let fs= require('fs');
+const path = require('path');
+const dirPath = path.resolve(__dirname);
+console.log("dir",dirPath)
 
-// 1. 读取图片
-fs.readFile()
+// // 1. 读取图片
+fs.readFile(dirPath+'/resource/a.jpg',(err,data) => {
+    if(!err) {
+        // console.log(data)
+        // 写入图片文件
+        fs.writeFile(__dirname+'/target/a.jpg',data, (err) => {
+            if(err) {throw err}
+            console.log("success")
+        })
+    }
+})
